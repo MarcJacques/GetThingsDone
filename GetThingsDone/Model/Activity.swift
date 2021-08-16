e904e97a0b29a1d5142e96d3a5964e3cbd541a38
Marc <marcjacques@me.com>
2021-08-16 08:41:31 -0400

    add Activity model

---
+++ GetThingsDone/Model/Activity.swift
@@ -0,0 +1,15 @@
+//
+//  Activity.swift
+//  GetThingsDone
+//
+//  Created by Marc Jacques on 8/16/21.
+//
+
+import Foundation
+
+struct Activity {
+    let name: String
+    var start: String
+    var stop: String
+    var reminderIntervals: Int
+}
